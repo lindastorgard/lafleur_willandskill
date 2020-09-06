641fdbde394c398c17d23a853b6d0b4f455627d1
lindastorgard <linda.storgard@gmail.com>
2020-09-06 22:44:13 +0200

    add _app page

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,14 @@
-import '../styles/globals.css'
+// import '../styles/globals.css'
+import * as React from "react";
+import { ThemeProvider } from "theme-ui";
+import theme from "../utils/theme";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <ThemeProvider theme={theme}>
+      <Component {...pageProps} />
+    </ThemeProvider>
+  )
 }
 
 export default MyApp
